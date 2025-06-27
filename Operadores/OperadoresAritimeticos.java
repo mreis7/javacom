6edf868195ae73d37bf697accafe1d1c2812319b
Matheus Reis <matheusreis.brr@gmail.com>
2025-06-27 13:32:12 -0300

    Operadores Aritimeticos

---
+++ Operadores/OperadoresAritimeticos.java
@@ -0,0 +1,18 @@
+public class OperadoresAritimeticos {
+  public static void main(String[] args) {
+    float n1 = 10;
+    float n2 = 20;
+
+    float multiplicacao = n1 * n2;
+    float divisao = n1 / n2;
+    float soma = n1 + n2;
+    float subtracao = n1 - n2;
+
+    System.out.println(" ");
+    System.out.println("Soma = " + soma);
+    System.out.println("Subtração = " + subtracao);
+    System.out.println("Multiplicaçõo = " + multiplicacao);
+    System.out.println("Divisão = " + divisao);
+    System.out.println(" ");
+  }
+}
